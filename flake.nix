a48e1b6d69647c0d2820603da3c9d4a91e22b2c3
LeixB <abone9999@gmail.com>
2022-06-16 20:06:47 +0200

    Add wsl shell

--- flake.nix
+++ flake.nix
@@ -17,7 +17,7 @@
     jdtls = pkgs.callPackage ./.jdtls.nix { };
   in
   rec {
-    devShell = with pkgs; mkShellNoCC {
+    devShells.default = with pkgs; mkShellNoCC {
       name = "java";
       buildInputs = [
         jdk jdtls # JDTLS requires java > 1.11
@@ -31,10 +31,21 @@
       KAFKA_TOPIC = "bdm_p2";
     };
 
+    devShells.wsl = with pkgs; mkShellNoCC {
+      name = "java";
+      buildInputs = [
+        apacheKafka
+        mongodb
+        mongodb-tools
+      ];
+
+      KAFKA_SERVER = "venomoth.fib.upc.edu:9092";
+      KAFKA_TOPIC = "bdm_p2";
+    };
+
     packages = {
       inherit jar;
       default = self.packages.${system}.jar;
     };
-
   });
 }
