afe2aee5bd7e899d655e3e182352bddeaefdd373
LeixB <abone9999@gmail.com>
2022-06-16 17:28:00 +0200

    Add remaining joins

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,20 +1,15 @@
 import java.util.HashMap;
-import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
-import java.util.Set;
 
 import com.mongodb.spark.MongoSpark;
 import com.mongodb.spark.config.ReadConfig;
-import com.mongodb.spark.rdd.api.java.JavaMongoRDD;
 
-import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.JavaSparkContext;
 import org.apache.spark.sql.Dataset;
 import org.apache.spark.sql.Row;
 import org.apache.spark.sql.SparkSession;
 import org.apache.spark.sql.functions;
-import org.bson.Document;
 
 public class Main {
 
@@ -26,73 +21,93 @@ public class Main {
         return MongoSpark.load(jsc, readConfig).toDF();
     }
 
-	public static void main(String[] args) {
-		SparkSession spark = SparkSession
-            .builder()
-            .master("local[*]")
-            .appName("P2")
-            .config("spark.mongodb.input.uri", "mongodb://127.0.0.1/test.income_opendata_neighborhood")
-            .config("spark.mongodb.output.uri", "mongodb://127.0.0.1/test.income_opendata_neighborhood")
-            .getOrCreate();
+    private static Dataset<Row> filterBelongs(Dataset<Row> data, final String column_id, final String column_list) {
+        final int neighborhood_column_index = data.schema().fieldIndex(column_id);
+        final int neighborhood_array_index = data.schema().fieldIndex(column_list);
+        return data.filter((x) -> {
+            List<String> ls = x.getList(neighborhood_array_index);
+            String id = x.getString(neighborhood_column_index);
+            return ls != null && id != null && ls.contains(id);
+        });
+    }
+
+    public static void main(String[] args) {
+        SparkSession spark = SparkSession
+                .builder()
+                .master("local[*]")
+                .appName("P2")
+                .config("spark.mongodb.input.uri", "mongodb://127.0.0.1/test.income_opendata_neighborhood")
+                .config("spark.mongodb.output.uri", "mongodb://127.0.0.1/test.income_opendata_neighborhood")
+                .getOrCreate();
         spark.sparkContext().setLogLevel("WARN");
         JavaSparkContext jsc = new JavaSparkContext(spark.sparkContext());
 
-		Dataset<Row> idealista = spark.read().parquet("./idealista/*");
+        Dataset<Row> idealista = spark.read().parquet("./idealista/*");
 
         Dataset<Row> rent_lookup_district = readCollection(jsc, "rent_lookup_district");
         Dataset<Row> rent_lookup_neighborhood = readCollection(jsc, "rent_lookup_neighborhood");
-        // Dataset<Row> income_lookup_district = readCollection(jsc, "income_lookup_district");
-        // Dataset<Row> income_lookup_neighborhood = readCollection(jsc, "income_lookup_neighborhood");
-        // Dataset<Row> income_opendata_neighborhood = readCollection(jsc, "income_opendata_neighborhood");
+        Dataset<Row> income_lookup_district = readCollection(jsc, "income_lookup_district");
+        Dataset<Row> income_lookup_neighborhood = readCollection(jsc, "income_lookup_neighborhood");
+        Dataset<Row> income_opendata_neighborhood = readCollection(jsc, "income_opendata_neighborhood");
 
         idealista.printSchema();
 
         rent_lookup_neighborhood.printSchema();
         Dataset<Row> nei = idealista
-            .join(rent_lookup_neighborhood.withColumnRenamed("_id", "nei_id"), idealista.col("neighborhood")
-            .equalTo(rent_lookup_neighborhood.col("ne")), "left_outer");
+                .join(rent_lookup_neighborhood.withColumnRenamed("_id", "nei_id"), idealista.col("neighborhood")
+                        .equalTo(rent_lookup_neighborhood.col("ne")), "left_outer");
 
         nei.printSchema();
 
         Dataset<Row> joined = nei
-            .join(rent_lookup_district, idealista.col("district")
-            .equalTo(rent_lookup_district.col("di")), "left_outer");
+                .join(rent_lookup_district, idealista.col("district").equalTo(rent_lookup_district.col("di")),
+                        "left_outer");
 
         joined.printSchema();
 
-        long nulls = joined.filter(joined.col("_id").isNull().and(joined.col("nei_id").isNull())).count();
-        System.out.println("Nulls: " + nulls + " / " + idealista.count());
+        // Check distribution of districts in idealista ordered
+        idealista.groupBy("district").count().orderBy(functions.desc("count")).show();
 
-        Set<String> districtSet = new HashSet<String>();
-        idealista
-            .select("district")
-            .distinct()
-            .collectAsList()
-            .forEach(row -> districtSet.add(row.getString(0)));
+        // Check that all the neighborhoods are in the correct district
+        Dataset<Row> idealista_correct = filterBelongs(joined, "nei_id", "ne_id");
 
-        // save districtSet to  file
-        districtSet.forEach(System.out::println);
+        System.out.println("OK: " + idealista_correct.count() + " / " + joined.count());
 
-        idealista.foreach(row -> {
-            System.out.println(row.getAs("_id") + " " + row.getAs("neighborhood") + " " + row.getAs("district"));
-        });
+        System.out.println("Income OpenData schema: ");
+        income_opendata_neighborhood.printSchema();
 
-        // rent_lookup_district.printSchema();
-        // long nulls = idealista.join(rent_lookup_district, idealista.col("district").isin(
-        //     rent_lookup_district.col("di"),
-        //     rent_lookup_district.col("di_n"),
-        //     rent_lookup_district.col("di_re")
-        // ), "left_outer").filter(functions.col("_id").isNull()).count();
+        System.out.println("Income Lookup neighborhood schema: ");
+        income_lookup_neighborhood.printSchema();
 
+        System.out.println("Income Lookup district schema: ");
+        income_lookup_district.printSchema();
 
-        // Create a JavaSparkContext using the SparkSession's SparkContext object
-        // JavaMongoRDD<Document> opendata = MongoSpark.load(jsc);
+        // For some reason neigh_name has an space at the end in the table...
+        Dataset<Row> income_joined = income_opendata_neighborhood
+                .withColumnRenamed("district_name", "district")
+                .join(income_lookup_district.withColumnRenamed("_id", "d_id"), "district")
+                .join(
+                        income_lookup_neighborhood.withColumnRenamed("_id", "n_id"),
+                        functions.col("neigh_name ").equalTo(functions.col("neighborhood")),
+                        "left_outer");
 
+        Dataset<Row> income_correct = filterBelongs(income_joined, "n_id", "neighborhood_id");
 
-        // Print the number of documents in the collection
-        // System.out.println("Number of documents in opendata: " + opendata.count());
-        // System.out.println("Number of documents in rent_lookup_district: " + rent_lookup_district.count());
+        System.out.println("OK: " + income_correct.count() + " / " + income_joined.count());
+
+        Dataset<Row> final_join = idealista_correct
+                .drop("district_name")
+                .join(income_joined, idealista_correct.col("nei_id").equalTo(income_joined.col("n_id")), "left_outer");
+
+        final_join.printSchema();
+
+        System.out.println(final_join.count() + " / " + joined.count());
+
+        // TODO: solve column name problem:
+        // Duplicate column(s): "neighborhood", "_id", "district" found, cannot save to
+        // file.
+        // final_join.write().parquet("./final_join");
 
         jsc.close();
-	}
+    }
 }
