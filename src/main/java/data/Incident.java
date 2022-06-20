f636dd9fd1c5b9d4f1531884af18b1f84d983270
LeixB <abone9999@gmail.com>
2022-06-21 00:49:59 +0200

    Improve incident parsing

--- src/main/java/data/Incident.java
+++ src/main/java/data/Incident.java
@@ -1,7 +1,6 @@
 package data;
 
 import java.io.Serializable;
-import java.util.function.Function;
 import java.util.stream.Stream;
 
 import org.apache.spark.sql.Row;
@@ -38,19 +37,36 @@ public class Incident implements Serializable {
         this.count = count;
     }
 
-    private <T> T parseFirstMatch(Row row, String[] fields) {
-        for (final String field : fields) {
-            final Integer fieldIndex = row.fieldIndex(field);
+    private int getIndex(Row row, String[] fields) {
+        for (int i = 0; i < fields.length; i++) {
+            final Integer fieldIndex = row.fieldIndex(fields[i]);
             if (fieldIndex != -1 && !row.isNullAt(fieldIndex))
-                return row.getAs(field);
+                return i;
         }
-        return null;
+        return -1;
+    }
+
+    private <T> T getValue(Row row, String field) {
+        return row.getAs(row.fieldIndex(field));
     }
 
     public Incident(Row row) {
-        this.neighborhood = parseFirstMatch(row, (new String[] { "Nom_barri", "Nom barri" }));
-        this.year = parseFirstMatch(row, (new String[] { "NK_Any", "NK Any" }));
-        this.count = parseFirstMatch(row, (new String[] {  "Numero_incidents_GUB", "Numero d'incidents GUB" }));
+        // There are two different kinds of datasets in the input, we need
+        // to determine which schema are we using.
+
+        final String[][] field_candidates = {
+            {"Nom_barri",            "Nom barri"},
+            {"NK_Any",               "NK Any"},
+            {"Numero_incidents_GUB", "Numero d'incidents GUB"},
+        };
+
+        // Since we know that once we get a match the rest will use the same
+        // schema, we can use the first match to determine the schema.
+        final int index = getIndex(row, field_candidates[0]);
+
+        this.neighborhood = getValue(row, field_candidates[0][index]);
+        this.year = getValue(row, field_candidates[1][index]);
+        this.count = getValue(row, field_candidates[2][index]);
     }
 
     // Helper function to filter codes
